a0b1a5b5261e6718635e11c57b23afc96834bfe2
Luca Nils Schmid <me@lucaschmid.net>
2017-05-25 20:18:22 +0200

    fix: escape alias before creating a regexp from it

--- src/index.js
+++ src/index.js
@@ -44,6 +44,11 @@ function getConfigPath(configPaths, findConfig) {
     return conf;
 }
 
+function escapeRegExpSpecialChars(str) {
+  // from https://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript/3561711#3561711
+    return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+
 export default function({ types: t }) {
     return {
         visitor: {
@@ -146,9 +151,8 @@ export default function({ types: t }) {
 
                 for(const aliasFrom in aliasConf) {
                     if(aliasConf.hasOwnProperty(aliasFrom)) {
-
                         let aliasTo = aliasConf[aliasFrom];
-                        const regex = new RegExp(`^${aliasFrom}(\/|$)`);
+                        const regex = new RegExp(`^${escapeRegExpSpecialChars(aliasFrom)}(\/|$)`);
 
                         // If the regex matches, replace by the right config
                         if(regex.test(filePath)) {
