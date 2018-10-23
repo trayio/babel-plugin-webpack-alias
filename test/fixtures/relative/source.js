86d4a9c74a368495a6364e4a65064bb6ec74729a
Nathan Friemel <nathan.friemel@gmail.com>
2018-10-23 15:09:32 -0500

    removing files I added but ended up not using

--- test/fixtures/relative/source.js
+++
@@ -1,9 +0,0 @@
-require('my-absolute-test-lib');
-require('my-absolute-test-lib-another');
-require('./my-absolute-test-lib');
-require('test/my-absolute-test-lib');
-require('same-folder-lib/my-absolute-test-lib');
-require('same-folder-lib');
-require('same-folder-lib/test/file');
-
-// Rest of the file
